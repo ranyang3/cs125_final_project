2488d178f484c2708452f006c337c1c378b1f4f5
mollyyang <tyy2@illinois.edu>
2019-04-28 15:49:09 -0500

    need changes

--- app/src/main/java/com/example/cs125_final_project/MainActivity.java
+++ app/src/main/java/com/example/cs125_final_project/MainActivity.java
@@ -1,14 +1,25 @@
 package com.example.cs125_final_project;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView today, goodjob;
+    Button add;
+    RecyclerView todos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        today = findViewById(R.id.today);
+        goodjob = findViewById(R.id.goodjob);
+        add = findViewById(R.id.add);
+        todos = findViewById(R.id.todos);
     }
 }
